327c4cfb2c6c359523ab0f8a8bcaa80361ee26a6
tahirbhalli <tahirbhalli16@gmail.com>
2020-07-22 11:05:51 +0500

    is draw

--- test/logic.test.js
+++ test/logic.test.js
@@ -6,3 +6,12 @@ test('is winner', () => {
 test('not winner', () => {
   expect(game.isWinner([1, 4, 3])).toBe(false);
 });
+test('draw', () => {
+  const cells = [];
+  for (let index = 0; index < 9; index += 1) {
+    const c = document.createElement('div');
+    c.innerText = 'X';
+    cells.push(c);
+  }
+  expect(game.isDraw(cells)).toBe(true);
+});
